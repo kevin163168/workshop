5692eb17b3b0d875b23bbd038d3a26e9df440f3e
Selvyn <swright@celestial.co.uk>
2021-10-27 21:25:56 +0100

    Tests and new controller added

--- src/test/java/com/celestial/firstspringbootapp/FirstSpringBootAppApplicationTests.java
+++ src/test/java/com/celestial/firstspringbootapp/FirstSpringBootAppApplicationTests.java
@@ -1,13 +1,20 @@
 package com.celestial.firstspringbootapp;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FirstSpringBootAppApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+class FirstSpringBootAppApplicationTests 
+{
+    @Autowired
+    WebController controller;
+    
+    @Test
+    void contextLoads() 
+    {
+        assertNotNull( controller );
+    }
 
 }
